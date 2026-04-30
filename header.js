c982cc1c19d3f387aa3af177b1db07074eeb7ee5
yayaaaas <alicia.yellet@gmail.com>
2026-04-30 14:44:27 +0200

    Update header.js

--- header.js
+++ header.js
@@ -2,20 +2,20 @@ document.addEventListener("DOMContentLoaded", function() {
     const headerHTML = `
     <header class="main-header">
         <div class="container_full">
-            <h1 class="logo"><a href="index.html">ALICI'ART.</a></h1>
+            <h1 class="logo"><a href="../index.html">ALICI'ART.</a></h1>
             <nav class="main-nav">
                 <ul>
-                    <li><a href="index.html">Accueil</a></li>
+                    <li><a href="../index.html">Accueil</a></li>
                     <li class="dropdown">
                         <a href="#" class="dropbtn">Projets ▾</a>
                         <ul class="dropdown-content">
-                            <li><a href="travaux.html" class="color-serrano-text">Travaux</a></li>
-                            <li><a href="photographies.html" class="color-habanero-text">Photographies</a></li>
-                            <li><a href="ecrits.html" class="color-chili-text">Écrits</a></li>
+                            <li><a href="../travaux.html" class="color-serrano-text">Travaux</a></li>
+                            <li><a href="../photographies.html" class="color-habanero-text">Photographies</a></li>
+                            <li><a href="../ecrits.html" class="color-chili-text">Écrits</a></li>
                         </ul>
                     </li>
-                    <li><a href="a_propos.html">À Propos</a></li>
-                    <li><a href="contact.html">Contact</a></li>
+                    <li><a href="../a_propos.html">À Propos</a></li>
+                    <li><a href="../contact.html">Contact</a></li>
                 </ul>
             </nav>
         </div>
@@ -23,7 +23,7 @@ document.addEventListener("DOMContentLoaded", function() {
     `;
     document.getElementById("header-placeholder").innerHTML = headerHTML;
 
-    // Ré-attache le script du menu mobile après l'injection
+    // Script pour le menu mobile
     const dropdown = document.querySelector('.dropdown');
     const dropbtn = document.querySelector('.dropbtn');
     if(dropbtn) {
